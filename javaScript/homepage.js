b27347c8ded83205024ddd621e445c050cb7e969
Ross M.H <rossmhynes@gmail.com>
2024-04-25 20:58:59 +0100

    updating the homepage to increment through each of the pages of beaches and make the info come from the top rather than the bottom

---
+++ javaScript/homepage.js
@@ -0,0 +1,20 @@
+const pages = ["slideshows/belhavenSlideshow.html", "slideshows/caytonSlideshow.html", "slideshows/croydeSlideshow.html", "slideshows/dalmoreSlideshow.html", "slideshows/fistralSlideshow.html", "slideshows/llangennithSlideshow.html", "slideshows/porthlevenSlideshow.html", "slideshows/portrushSlideshow.html", "slideshows/thursoSlideshow.html", "slideshows/tullanSlideshow.html"]
+
+let currentPageIndex = 0;
+	
+function loadPage(pageIndex) {
+	const iframe = document.getElementById("embeddedPage");
+	iframe.src = pages[pageIndex];
+}
+
+function nextPage() {
+	currentPageIndex = currentPageIndex + 1;
+	loadPage(currentPageIndex);
+}
+
+function prevPage() {
+	currentPageIndex = currentPageIndex - 1;
+	loadPage(currentPageIndex);
+}
+
+loadPage(currentPageIndex);
