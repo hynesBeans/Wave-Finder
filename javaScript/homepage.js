4a17e5ee95ca2deb881fa4da4ad936d252a22d99
Ross M.H <rossmhynes@gmail.com>
2024-04-26 10:52:33 +0100

    adding internal commentary

--- javaScript/homepage.js
+++ javaScript/homepage.js
@@ -1,20 +1,37 @@
+/* Adding the html files for each one that wants to be implemented */
 const pages = ["slideshows/belhavenSlideshow.html", "slideshows/caytonSlideshow.html", "slideshows/croydeSlideshow.html", "slideshows/dalmoreSlideshow.html", "slideshows/fistralSlideshow.html", "slideshows/llangennithSlideshow.html", "slideshows/porthlevenSlideshow.html", "slideshows/portrushSlideshow.html", "slideshows/thursoSlideshow.html", "slideshows/tullanSlideshow.html"]
 
 let currentPageIndex = 0;
-	
+
+/* Load the page to be embedded on the homepage */
 function loadPage(pageIndex) {
 	const iframe = document.getElementById("embeddedPage");
 	iframe.src = pages[pageIndex];
 }
 
-function nextPage() {
-	currentPageIndex = (currentPageIndex + 1) % pages.length;
-	loadPage(currentPageIndex);
-}
-
+/* Changing the current page to be 1 previous to the one initially loaded */
 function prevPage() {
 	currentPageIndex = (currentPageIndex - 1 + pages.length) % pages.length;
 	loadPage(currentPageIndex);
 }
 
+/* Changing the current page to be 1 next to the one initially loaded */
+function nextPage() {
+	currentPageIndex = (currentPageIndex + 1) % pages.length;
+	loadPage(currentPageIndex);
+}
+
+/* Would have been the code to load any beaches that were liked into the liked beaches page */
+/*function likePage() {
+	var listContainer = document.getElementById("likedBeachList");
+	var newFilePath = pages[index];
+	
+	listContainer.innerHTML = "<<iframe id="embeddedPage" src='" + newFilePath + "'></iframe>";
+	
+	likeBeachList.forEach(function(file, index)) {
+		listContainer.innerHTML += "<<iframe id="embeddedPage" src='" + newFilePath + "'></iframe> <br><br>";
+	}
+}*/
+
+/* Initially load the first embedded file onto the homepage */
 loadPage(currentPageIndex);
--- slideshows/slideshow.js
+++ slideshows/slideshow.js
@@ -1,21 +1,25 @@
 let slideIndex = 1;
 
+/* Gain the initial photo/slide */
 showSlides(slideIndex);
 
-
+/* Call the function to gain the next or previous slide */
 function plusSlides(n) {
 	showSlides(slideIndex += n);
 }
 
+/* Call the function ot gain the slide from usage of dot buttons */
 function currentSlide(n) {
 	showSlides(slideIndex = n);
 }
 
+/* Function to determine what image is sent to display */
 function showSlides(n) {
 	let i;
 	let slides = document.getElementsByClassName("mySlides");
 	let dots = document.getElementsByClassName("dot");
 	
+	/* Checking for first and last items in the array and looping back to beginning or end */
 	if (n > slides.length) {
 		slideIndex = 1
 	}
@@ -23,6 +27,7 @@ function showSlides(n) {
 		slideIndex = slides.length
 	}
 	
+	/* Deactivate other dots and images */
 	for (i = 0; i < slides.length; i++) {
 		slides[i].style.display = "none";  
 	}
@@ -30,6 +35,7 @@ function showSlides(n) {
 		dots[i].className = dots[i].className.replace(" active", "");
 	}
 	
+	/* Activate the wanted image and dot */
 	slides[slideIndex-1].style.display = "block";  
 	dots[slideIndex-1].className += " active";
 }
